26305766faecda76d480d3dbcc9856049e0b29ec
Hamzaan Bridle <hamzaan.bridle@bjss.com>
2020-12-07 18:47:03 +0000

    Unit test changes. CBA with BaggageTypeParserTest.java

--- 2020/src/test/java/day7/BaggageTypeTest.java
+++ 2020/src/test/java/day7/BaggageTypeTest.java
@@ -2,6 +2,8 @@ package day7;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
@@ -50,4 +52,25 @@ class BaggageTypeTest {
         then(bagRule).isPresent();
         then(bagRule.get()).isEqualTo(expected);
     }
+
+    @Test
+    void testGetBagRules() {
+        // Given
+        String colour = "red";
+        BaggageType baggageType = new BaggageType(colour);
+        String colourToFind = "Blue";
+        BaggageType typeInRule = new BaggageType(colourToFind);
+        BagRule expectedRule = new BagRule(typeInRule, 5);
+        baggageType.addBagRule(typeInRule, 5);
+
+        // When
+        List<BagRule> bagRules = baggageType.getBagRules();
+
+        // Then
+        then(bagRules).isNotNull();
+        then(bagRules).isEqualTo(Collections.singletonList(expectedRule));
+
+
+    }
+
 }
