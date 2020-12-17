cc17d1f7f6373d5c48a9a4813e253910be282152
Hamzaan Bridle <hamzaan.bridle@bjss.com>
2020-12-17 09:01:44 +0000

    Tidy up

--- 2020/src/main/java/day16/TicketDecoder.java
+++ 2020/src/main/java/day16/TicketDecoder.java
@@ -1,17 +1,14 @@
 package day16;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import javax.swing.*;
 
 public class TicketDecoder {
 
